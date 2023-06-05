95ecab15858a94b8004a8643188a98aa0c3a2615
Jafarali233 <135589067+Jafarali233@users.noreply.github.com>
2023-06-05 19:28:19 +0700

    Add files via upload

---
+++ jafar_233.java
@@ -0,0 +1,13 @@
+
+public class jafar_233 {
+
+
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("======================================");
+        System.out.println("Nama  : Jafar Ali Saifudin");
+        System.out.println("NIM   : 2113010233");
+        System.out.println("Kelas : 4F ");
+        System.out.println("======================================");
+    }
+}
