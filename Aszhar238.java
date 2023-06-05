506d04db9baa81eb814eb757fb4c6a8a0af91ded
Aszharmahesakyun <133620996+Aszharmahesakyun@users.noreply.github.com>
2023-06-05 19:39:07 +0700

    Add files via upload

---
+++ Aszhar238.java
@@ -0,0 +1,12 @@
+public class Aszhar238 {
+
+
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("======================================");
+        System.out.println("Nama  : Aszhar Mahesakayun");
+        System.out.println("NIM   : 2113010238");
+        System.out.println("Kelas : 4F ");
+        System.out.println("======================================");
+    }
+}
