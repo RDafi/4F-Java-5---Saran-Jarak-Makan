1bd0a8853e29ada3f387758e0f2a6c1bf7ae6f25
Dodik241 <135590170+Dodik241@users.noreply.github.com>
2023-06-06 18:57:44 +0700

    Add files via upload

---
+++ Dodik241.java
@@ -0,0 +1,11 @@
+public class Dodik241 {
+
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("======================================");
+        System.out.println("Nama  : Dodik Yoga Pratama Putra");
+        System.out.println("NIM   : 2113010241");
+        System.out.println("Kelas : 4F ");
+        System.out.println("======================================");
+    }
+}
