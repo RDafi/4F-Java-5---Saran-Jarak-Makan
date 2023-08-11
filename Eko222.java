7165f9627d03d02519a49db7c4fc789fadebf336
ekonugroho22 <134469910+ekonugroho22@users.noreply.github.com>
2023-08-11 22:08:27 +0700

    Create Eko222.java

---
+++ Eko222.java
@@ -0,0 +1,13 @@
+
+public class eko_222 {
+
+
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("======================================");
+        System.out.println("Nama  : Eko Nugroho");
+        System.out.println("NIM   : 2113010222");
+        System.out.println("Kelas : 4F ");
+        System.out.println("======================================");
+    }
+}
